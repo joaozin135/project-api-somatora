bceff49da957da1b0e294b8138c2a77e524893b1
João Junior <junior.joao1305@gmail.com>
2026-08-21 14:34:50 -0300

    feature: role for user

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,11 +1,39 @@
-import { IsEmail, IsNotEmpty, Isstring, MinLength } from 'class-validator';
+import { IsEmail, 
+  IsString, 
+  IsStrongPassword,
+  MinLength,
+  IsEnum, 
+} from 'class-validator';
+
+import { ApiProperty } from '@nestjs/swagger';
+import { Role } from 'src/generated/prisma/browser';
 
 export class CreateUserDTO {
-@IsInt({ message: 'Id precisa ser numero' })
-id: number;
-@IsString({message: })
-@MinLength(2)
-name!: string;
-@IsEmail()
-email!: string;
+  @IsString({ message: 'Nome é obrigatório' })
+  @MinLength(2)
+  @ApiProperty({
+    description: 'Nome do usuário',
+    example: 'João da Silva',
+  })
+  name!: string;
+  @IsEmail()
+  @ApiProperty({
+    description: 'Email do usuário',
+    example: 'joao.silva@example.com',
+  })
+  email!: string;
+  @IsStrongPassword({
+    minLength: 8,
+  })
+  @ApiProperty({
+    description: 'Senha do usuário',
+    example: 'SenhaForte123!',
+  })
+  password!: string;
+  @IsEnum(Role)
+  @ApiProperty ({
+    description: 'Tipo de acesso do usuário',
+    example: 'ADMIN',
+  })
+  role!: Role;
 }
