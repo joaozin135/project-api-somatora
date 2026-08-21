affe91baf28ad0a63360742be8be0449df192523
João Junior <junior.joao1305@gmail.com>
2026-08-21 17:45:59 -0300

    chore: update providers

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,10 +2,17 @@ import { Module } from '@nestjs/common';
 import { UserController } from './users.controller';
 import { UsersService } from './users.service';
 import { PrismaModule } from '../prisma/prisma.module';
+import { UserRepository } from './repositories/user-repository';
+import { USER_REPOSITORY } from './repositories/user-repository.interface';
 
 @Module({
   imports: [PrismaModule],
   controllers: [UserController],
-  providers: [UsersService]
+  providers: [
+    UsersService, {
+      provide: USER_REPOSITORY,
+      useClass: UserRepository,
+    }
+  ]
 })
 export class UsersModule {}
