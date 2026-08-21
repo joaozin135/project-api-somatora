98df24b75c5f2e34495aa1e1855122c381504cf4
João Junior <junior.joao1305@gmail.com>
2026-08-21 14:26:42 -0300

    feat(users): add REST controller with POST and GET endpoints

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,36 @@
-import { Controller } from '@nestjs/common';
 
-@Controller('users')
-export class UsersController {}
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateUserDTO } from './dto/create-user.dto';
+
+@Controller('user')
+export class UserController {
+    constructor(private readonly usersService: UsersService) {}
+        
+        @Post()
+        create(@Body() createUserDTO: CreateUserDTO) {
+            return this.usersService.create(createUserDTO);
+        }         
+
+        @Get()
+        findAll(){
+            return this.usersService.findAll();
+        }
+
+        @ Get(':id')
+        findOne(@Param('id') id: string){
+            return this.usersService.findOne(id);
+        }
+}
