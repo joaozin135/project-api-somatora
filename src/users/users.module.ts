fa66ab05f7a97ad483f360070f900bdbcd06e3d2
João Junior <junior.joao1305@gmail.com>
2026-08-21 14:36:04 -0300

    feature: imports correction userController

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
-import { UsersController } from './users.controller';
+import { UserController } from './users.controller';
 import { UsersService } from './users.service';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
-  controllers: [UsersController],
+  imports: [PrismaModule],
+  controllers: [UserController],
   providers: [UsersService]
 })
 export class UsersModule {}
