c509b20455021784613a4e3d46b9dc0e6f8bfc02
João Junior <junior.joao1305@gmail.com>
2026-08-28 16:37:18 -0300

    chore: add Receipt module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
+import { ReceiptModule } from './receipt/receipt.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot ({ isGlobal: true }),
-    UsersModule
+    UsersModule,
+    ReceiptModule
   ],
   controllers: [AppController],
   providers: [AppService],
