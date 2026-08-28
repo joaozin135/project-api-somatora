cda5c8aab4008e7360d9297f81b85236c048dcf5
João Junior <junior.joao1305@gmail.com>
2026-08-28 17:32:49 -0300

    feature: implementation methods find and delete

--- src/receipt/receipt.controller.ts
+++ src/receipt/receipt.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
 import { ReceiptService } from './receipt.service';
 import { CreateReceiptsDTO } from './dto/create-receipts.dto';
 
@@ -9,5 +9,20 @@ export class ReceiptController {
   @Post()
   create(@Body() createReceiptDTO: CreateReceiptsDTO) {
     return this.receiptService.create(createReceiptDTO);
+  }
+
+  @Get()
+  findAll() {
+    return this.receiptService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.receiptService.findOne(id);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.receiptService.remove(id);
   }
 }
--- src/receipt/receipt.service.ts
+++ src/receipt/receipt.service.ts
@@ -1,8 +1,14 @@
-import { Inject, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Inject,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateReceiptsDTO } from './dto/create-receipts.dto';
 import { RECEIPT_REPOSITORY } from './repositories/receipt-repository.interface';
 import type { IReceiptRepository } from './repositories/receipt-repository.interface';
- 
+import { UpdateReceiptsDTO } from './dto/update-receipts.dto';
+
 @Injectable()
 export class ReceiptService {
   constructor(
@@ -17,4 +23,43 @@ export class ReceiptService {
     });
     return newReceipt;
   }
+
+  async findAll() {
+    return this.receiptRepository.findAll();
+  }
+
+  async findOne(id: string) {
+    const receipt = await this.receiptRepository.findById(id);
+    if (!receipt)
+      throw new NotFoundException(`Recebimento com id ${id} não encontrado`);
+    return receipt;
+  }
+
+  async update(id: string, { type, diameterClass }: UpdateReceiptsDTO) {
+    await this.findOne(id);
+
+    if (type === undefined && diameterClass === undefined) {
+      throw new BadRequestException(
+        'Informe ao menos um campo para atualizar.',
+      );
+    }
+
+    if (type !== undefined) {
+      const updatedReceipt = await this.receiptRepository.update(id, {
+        type,
+      });
+    }
+
+    if (diameterClass !== undefined) {
+      const updatedReceipt = await this.receiptRepository.update(id, {
+        diameterClass,
+      });
+    }
+  }
+
+  async remove(id: string){
+    await this.receiptRepository.remove(id);
+  }
+
+
 }
