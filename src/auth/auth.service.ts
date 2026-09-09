fdf1cce60c7554878a24ca9b0cb654e4aa1dd0d2
João Junior <junior.joao1305@gmail.com>
2026-09-09 17:24:10 -0300

    chore: include role in the JWT payload

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -31,6 +31,7 @@ export class AuthService {
       sub: user.id,
       email: user.email,
       name: user.name,
+      role: user.role,
     };
 
     const accessToken = await this.jwtService.signAsync(payload);
